07c5a6271692358abed48e784c3f2e82cc46dac7
KyleWhen <barker.kyle17@gmail.com>
2020-11-23 12:57:44 -0500

    1 file modified

--- js/SignUp.js
+++ js/SignUp.js
@@ -24,22 +24,3 @@ $('#signup-form').submit(function(e){
     console.log(error.message);
   });
 });
-
-/*$('#signup-form').submit(function(e){
-  e.preventDefault();
-  console.log("click the submit");
-
-  // use the provided sign in 
-  var email = "yilianz@gmail.com";
-  var psw = "aiken";
-  firebase.auth().createUserWithEmailAndPassword(email,psw).then(user =>{
-       console.log("success");
-  }).catch(error=>{
-       console.log(error.code); 
-       console.log(error.message);
-  });
-
-
-
-});*/
-
