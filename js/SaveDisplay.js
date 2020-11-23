47ecc8f8be4a94d3ac194986982fa34893fc78c7
KyleWhen <barker.kyle17@gmail.com>
2020-11-23 12:57:25 -0500

    2 files modified, 2 added

--- js/SaveDisplay.js
+++ js/SaveDisplay.js
@@ -1,6 +1,14 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {};
+var firebaseConfig = {
+  apiKey: "AIzaSyAQ3-DuFMINqU412qUeua5WMZZPU1l1EuU",
+  authDomain: "grocerylist-bde77.firebaseapp.com",
+  databaseURL: "https://grocerylist-bde77.firebaseio.com",
+  projectId: "grocerylist-bde77",
+  storageBucket: "grocerylist-bde77.appspot.com",
+  messagingSenderId: "139163594125",
+  appId: "1:139163594125:web:36edde353ef2c80e4861c4"
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
---
+++ js/SignUp.js
@@ -0,0 +1,45 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAQ3-DuFMINqU412qUeua5WMZZPU1l1EuU",
+  authDomain: "grocerylist-bde77.firebaseapp.com",
+  databaseURL: "https://grocerylist-bde77.firebaseio.com",
+  projectId: "grocerylist-bde77",
+  storageBucket: "grocerylist-bde77.appspot.com",
+  messagingSenderId: "139163594125",
+  appId: "1:139163594125:web:36edde353ef2c80e4861c4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+console.log("start...");
+
+$('#signup-form').submit(function(e){
+  e.preventDefault();
+  console.log("click the submit...");
+  var email = "testemail@gmail.com";
+  var password = "testtest";
+  firebase.auth().createUserWithEmailAndPassword(email,password).then(user =>{
+    console.log("success!");
+  }).catch(error =>{
+    console.log(error.code);
+    console.log(error.message);
+  });
+});
+
+/*$('#signup-form').submit(function(e){
+  e.preventDefault();
+  console.log("click the submit");
+
+  // use the provided sign in 
+  var email = "yilianz@gmail.com";
+  var psw = "aiken";
+  firebase.auth().createUserWithEmailAndPassword(email,psw).then(user =>{
+       console.log("success");
+  }).catch(error=>{
+       console.log(error.code); 
+       console.log(error.message);
+  });
+
+
+
+});*/
+
